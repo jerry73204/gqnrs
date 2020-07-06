ad173081a062b2ae3343325a8a5bcb88032671dd
jerry73204 <jerry73204@gmail.com>
2020-07-06 23:38:53 +0800

    WIP on training pipeline

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use gqnrs::{
     config::{Config, DatasetConfig, DeepMindDatasetConfig},
     dataset,
     message::{WorkerAction, WorkerResponse},
-    model::{GqnModel, GqnModelInput, GqnModelOutput, TowerEncoder},
+    model::{GqnModelInit, GqnModelInput, GqnModelOutput},
 };
 
 lazy_static::lazy_static! {
@@ -41,7 +41,7 @@ async fn main() -> Fallible<()> {
     }
 
     // Load dataset
-    info!("Loading dataset");
+    info!("loading dataset");
 
     let (dataset, frame_channels, param_channels) = match config.dataset {
         DatasetConfig::DeepMind(dataset_config) => {
@@ -76,9 +76,18 @@ async fn main() -> Fallible<()> {
         }
     };
 
+    info!("dataset is ready");
+
     // spawn training workers
-    struct UploadMessage {}
+    #[derive(Debug)]
+    struct UploadMessage {
+        worker_index: usize,
+        output: GqnModelOutput,
+        grads: Vec<Tensor>,
+    }
+    #[derive(Debug, Clone)]
     struct DownloadMessage {}
+    #[derive(Debug)]
     struct DataMessage {
         pub step: usize,
         pub input: GqnModelInput,
@@ -87,7 +96,7 @@ async fn main() -> Fallible<()> {
     let num_workers = config.training.devices.len();
     let (upload_tx, upload_rx) = mpsc::channel::<UploadMessage>(num_workers);
     let (download_tx, _download_rx) = broadcast::channel::<DownloadMessage>(num_workers);
-    let mut upload_rx = Some(upload_rx);
+    let mut upload_rx_opt = Some(upload_rx);
     let (mut data_tx_set, mut data_rx_set) = (0..num_workers).fold(
         (vec![], HashMap::new()),
         |(mut tx_set, mut rx_set), worker_index| {
@@ -99,20 +108,17 @@ async fn main() -> Fallible<()> {
     );
 
     let feed_future = async move {
+        info!("starded feeding worker");
         let mut train_stream = Box::pin(dataset.train_stream(0)?); // TODO: initial step
         let mut step: usize = 0;
 
         loop {
-            let (train_stream_, inputs) = futures::stream::iter(0..num_workers)
-                .fold(Ok((train_stream, vec![])), |result, _| {
-                    async move {
-                        let (mut stream, mut inputs) = result?;
-                        inputs.push(stream.next().await.unwrap()?);
-                        Fallible::Ok((stream, inputs))
-                    }
-                })
-                .await?;
-            train_stream = train_stream_;
+            info!("step: {}", step);
+
+            let mut inputs = vec![];
+            for _ in 0..num_workers {
+                inputs.push(train_stream.next().await.unwrap()?);
+            }
 
             for (data_tx, input) in data_tx_set.iter_mut().zip(inputs.into_iter()) {
                 if let Err(_) = data_tx.send(DataMessage { step, input }).await {
@@ -137,51 +143,134 @@ async fn main() -> Fallible<()> {
         .map(ToOwned::to_owned)
         .enumerate()
         .map(|(worker_index, device)| {
+            info!("starded training worker {}", worker_index);
             let is_master = worker_index == 0;
             let mut data_rx = data_rx_set.remove(&worker_index).unwrap();
-            let (mut upload_tx_opt, mut upload_rx_opt, mut download_tx_opt, mut download_rx_opt) =
-                if is_master {
-                    (
-                        None,
-                        Some(upload_rx.take().unwrap()),
-                        Some(download_tx.clone()),
-                        None,
-                    )
-                } else {
-                    (
-                        Some(upload_tx.clone()),
-                        None,
-                        None,
-                        Some(download_tx.subscribe()),
-                    )
-                };
+            let mut upload_tx = upload_tx.clone();
+            let mut upload_rx_opt = if is_master {
+                Some(upload_rx_opt.take().unwrap())
+            } else {
+                None
+            };
+            let mut download_tx_opt = if is_master {
+                Some(download_tx.clone())
+            } else {
+                None
+            };
+            let mut download_rx = download_tx.subscribe();
 
             async move {
                 let vs = VarStore::new(device);
                 let model = {
                     let root = vs.root();
-                    let model = GqnModel::<TowerEncoder>::new(
-                        &root,
-                        frame_channels as i64,
-                        param_channels as i64,
-                    );
+                    let model = GqnModelInit::new(frame_channels as i64, param_channels as i64)
+                        .build(&root);
                     let _ = root.zeros("step", &[]);
                     model
                 };
+                let trainable_variables = vs.trainable_variables();
 
                 while let Some(data_msg) = data_rx.recv().await {
                     let DataMessage { step, input } = data_msg;
+                    let input = input.to_device(device);
+                    let output = model(&input, true);
 
-                    let output = model.forward_t(input, true);
+                    let mean_elbo_loss = output.elbo_loss.mean(Kind::Float);
 
-                    // TODO
+                    // compute gradient
 
+                    // DEBUG
+                    vs.variables()
+                        .into_iter()
+                        .enumerate()
+                        .for_each(|(index, (name, tensor))| {
+                            println!("{}\t{}\t{:?}", index, name, tensor);
+                        });
+
+                    let _grads = Tensor::run_backward(
+                        &[mean_elbo_loss],
+                        &trainable_variables,
+                        // &vs.variables()
+                        //     .into_iter()
+                        //     .map(|(_, tensor)| tensor)
+                        //     .collect::<Vec<_>>(),
+                        true,
+                        true,
+                    );
+
+                    let grads = trainable_variables
+                        .iter()
+                        .map(|tensor| tensor.grad().detach().copy())
+                        .collect::<Vec<_>>();
+
+                    // upload to master
+                    {
+                        let upload_msg = UploadMessage {
+                            worker_index,
+                            output,
+                            grads,
+                        };
+                        if let Err(_) = upload_tx.send(upload_msg).await {
+                            panic!("please report bug");
+                        }
+                    }
+
+                    // process outcomes in master
                     if is_master {
                         let upload_rx = upload_rx_opt.as_mut().unwrap();
                         let download_tx = download_tx_opt.as_mut().unwrap();
-                    } else {
-                        let upload_tx = upload_tx_opt.as_mut().unwrap();
-                        let download_rx = download_rx_opt.as_mut().unwrap();
+
+                        let instant = Instant::now();
+                        let upload_msgs = {
+                            let mut upload_msgs = vec![];
+                            for _ in 0..num_workers {
+                                upload_msgs.push(upload_rx.recv().await.unwrap());
+                            }
+                            upload_msgs
+                        };
+
+                        // DEBUG
+                        {
+                            let num_msgs = upload_msgs.len();
+                            let num_grads = upload_msgs[0].grads.len();
+
+                            for grad_index in 0..num_grads {
+                                for msg_index in 0..num_msgs {
+                                    let grad = &upload_msgs[msg_index].grads[grad_index];
+                                    dbg!(msg_index, grad_index, grad);
+                                }
+                            }
+                        }
+
+                        // upload_msgs
+                        //     .iter()
+                        //     .map(|msg| {
+                        //         msg.grads
+                        //             .iter()
+                        //             .enumerate()
+                        //             .map(|(index, tensor)| {
+                        //                 let tensor = tensor.clone();
+                        //                 dbg!(index, tensor.size());
+                        //                 let tensor = tensor.to_device(device);
+                        //                 tensor
+                        //             })
+                        //             .collect::<Vec<_>>()
+                        //     })
+                        //     .collect::<Vec<_>>();
+
+                        // upload_msgs.iter().map(|msg| msg.output.to_device(device));
+
+                        // TODO
+
+                        let download_msg = DownloadMessage {};
+                        if let Err(_) = download_tx.send(download_msg) {
+                            panic!("please report bug");
+                        }
+                    }
+
+                    // download from master
+                    {
+                        let download_msg = download_rx.recv().await.unwrap();
                     }
                 }
 
@@ -757,5 +846,21 @@ fn combine_gqn_outputs(outputs: Vec<GqnModelOutput>, target_device: Device) -> G
 //                 return;
 //             }
 //         };
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn grad_test() {
+//         let vs = VarStore::new(Device::cuda_if_available());
+//         let root = vs.root();
+//         let x = root.var("x", &[], Init::KaimingUniform);
+//         let y = root.var("y", &[], Init::KaimingUniform);
+//         let z = &x * &y;
+//         let grads = Tensor::run_backward(&[&z], &[&x, &y], true, true);
+//         dbg!(grads);
 //     }
 // }
